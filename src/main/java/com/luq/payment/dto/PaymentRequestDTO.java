785b8134ebf4242f7697882be2621d5614c5343b
lucas mateus <lucas.mts1812@gmail.com>
2025-09-02 12:11:10 -0300

    Added order id on PaymentRequestDTO

--- src/main/java/com/luq/payment/dto/PaymentRequestDTO.java
+++ src/main/java/com/luq/payment/dto/PaymentRequestDTO.java
@@ -3,7 +3,8 @@ package com.luq.payment.dto;
 import java.math.BigDecimal;
 
 public record PaymentRequestDTO (
-        BigDecimal unitPrice,
-        Integer quantity,
-        String product_name
+    String orderId,
+    BigDecimal unitPrice,
+    Integer quantity,
+    String product_name
 ){}
