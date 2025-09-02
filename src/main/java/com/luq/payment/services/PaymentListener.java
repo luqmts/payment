bc3bb2c7086e54e39e6a4bb84ca096674c7b6f29
lucas mateus <lucas.mts1812@gmail.com>
2025-09-02 12:09:38 -0300

    Removed unused imports

--- src/main/java/com/luq/payment/services/PaymentListener.java
+++ src/main/java/com/luq/payment/services/PaymentListener.java
@@ -2,7 +2,6 @@ package com.luq.payment.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.luq.payment.dto.PaymentRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -16,8 +15,6 @@ public class PaymentListener {
     @KafkaListener(topics = "payment-topic", groupId = "payment-group")
     public void process(String message) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-
         PaymentRequestDTO data = mapper.readValue(message, PaymentRequestDTO.class);
         paymentService.createCheckout(data);
     }
