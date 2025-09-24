9c7eeabd5cc87f8702bae5407020a2d5a5110184
lucas mateus <lucas.mts1812@gmail.com>
2025-09-24 14:14:27 -0300

    feat: Updated StripePaymentGateway

--- src/main/java/com/luq/payment/domain/StripePaymentGateway.java
+++ src/main/java/com/luq/payment/domain/StripePaymentGateway.java
@@ -1,25 +1,22 @@
 package com.luq.payment.domain;
 
 import com.luq.payment.dto.PaymentRequestDTO;
+import com.luq.payment.dto.SessionDTO;
+import com.luq.payment.exceptions.StripeErrorException;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class StripePaymentGateway{
-    public Map<String, String> createCheckoutSession(
-        PaymentRequestDTO request,
-        String successUrl
-    ) {
+    public SessionDTO createCheckoutSession(PaymentRequestDTO request) {
         SessionCreateParams params = SessionCreateParams.builder()
             .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
             .setMode(SessionCreateParams.Mode.PAYMENT)
-            .setSuccessUrl(successUrl)
+            .setSuccessUrl(System.getenv("SUCCESS_URL"))
             .setClientReferenceId(request.orderId())
             .setExpiresAt((System.currentTimeMillis() / 1000L) + 1800)
             .addLineItem(
@@ -27,24 +24,22 @@ public class StripePaymentGateway{
                 .setQuantity(request.quantity().longValue())
                 .setPriceData(
                     SessionCreateParams.LineItem.PriceData.builder()
-                    .setCurrency("brl")
+                    .setCurrency("BRL")
                     .setUnitAmount(request.unitPrice().multiply(BigDecimal.valueOf(100)).longValue())
                     .setProductData(
                         SessionCreateParams.LineItem.PriceData.ProductData.builder()
-                            .setName(request.product_name())
+                            .setName(request.productName())
                             .build()
                     ).build()
                 ).build()
             ).build();
+
         try {
             Session session = Session.create(params);
-            Map<String, String> responseData = new HashMap<>();
-            responseData.put("checkoutUrl", session.getUrl());
-            return responseData;
-        } catch (StripeException ex) {
-            ex.printStackTrace();
-        }
 
-        return null;
+            return new SessionDTO(session.getId(), session.getStatus(), session.getUrl());
+        } catch (StripeException ex) {
+            throw new StripeErrorException(ex.getMessage());
+        }
     }
 }
