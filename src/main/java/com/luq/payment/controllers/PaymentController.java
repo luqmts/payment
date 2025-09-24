82474afba837b30da447f82409dc23502efc4d5a
lucas mateus <lucas.mts1812@gmail.com>
2025-09-24 14:19:34 -0300

    feat: Updated PaymentService

--- src/main/java/com/luq/payment/controllers/PaymentController.java
+++ src/main/java/com/luq/payment/controllers/PaymentController.java
@@ -3,12 +3,11 @@ package com.luq.payment.controllers;
 import com.luq.payment.dto.PaymentRequestDTO;
 import com.luq.payment.dto.SessionDTO;
 import com.luq.payment.services.PaymentService;
-import com.stripe.model.checkout.SessionCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -17,15 +16,15 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping("/create-checkout")
-    public ResponseEntity<Map<String, String>> processPayment(@RequestBody PaymentRequestDTO request) {
-        Map<String, String> response = paymentService.createCheckout(request);
+    public ResponseEntity<SessionDTO> processPayment(@RequestBody PaymentRequestDTO request) {
+        SessionDTO response = paymentService.createCheckout(request);
 
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/get-all-sessions")
-    public ResponseEntity<SessionCollection> getAllSessions() {
-        SessionCollection response = paymentService.getAllSessions();
+    @GetMapping("/get-last-sessions")
+    public ResponseEntity<List<SessionDTO>> getLastSessions() {
+        List<SessionDTO> response = paymentService.getLastSessions();
 
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/luq/payment/services/PaymentService.java
+++ src/main/java/com/luq/payment/services/PaymentService.java
@@ -4,51 +4,57 @@ import com.luq.payment.domain.*;
 import com.luq.payment.dto.PaymentRequestDTO;
 import com.luq.payment.dto.SessionDTO;
 import com.luq.payment.exceptions.NegativeQuantityException;
+import com.luq.payment.exceptions.NotFoundException;
+import com.luq.payment.exceptions.StripeErrorException;
 import com.stripe.exception.InvalidRequestException;
 import com.stripe.exception.StripeException;
 import com.stripe.model.checkout.Session;
-import com.stripe.model.checkout.SessionCollection;
 import com.stripe.param.checkout.SessionListParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.util.Map;
+import java.util.List;
 
 @Service
 public class PaymentService {
     @Autowired
     private StripePaymentGateway paymentGateway;
 
-    public Map<String, String> createCheckout(PaymentRequestDTO data) {
+    public SessionDTO createCheckout(PaymentRequestDTO data) {
         if (data.quantity() <= 0)
             throw new NegativeQuantityException("Quantity must be greater or equal than 1");
 
         if (data.unitPrice().compareTo(BigDecimal.ZERO) == 0)
             throw new NegativeQuantityException("Unit price must be greater or equal than 1");
-        return paymentGateway.createCheckoutSession(
-            data,
-            "http://localhost:8080/order/list"
-        );
+
+        return paymentGateway.createCheckoutSession(data);
     }
 
-    public SessionCollection getAllSessions() {
+    public List<SessionDTO> getLastSessions() {
         try {
-            SessionListParams params = SessionListParams.builder().setLimit(3L).build();
-            return Session.list(params);
-        } catch (StripeException e) {
-            throw new RuntimeException(e);
+            SessionListParams params = SessionListParams.builder().setLimit(5L).build();
+
+            return Session
+                .list(params)
+                .getData()
+                .stream()
+                .map(data -> new SessionDTO(data.getId(), data.getStatus(), data.getUrl()))
+                .toList();
+        } catch (StripeException ex) {
+            throw new StripeErrorException(ex.getMessage());
         }
     }
 
     public SessionDTO getSessionInfo(String session_id) {
         try {
             Session session = Session.retrieve(session_id);
+
             return new SessionDTO(session.getId(), session.getStatus(), session.getUrl());
         } catch (InvalidRequestException ex) {
-            throw new RuntimeException("Invalid session's id");
+            throw new NotFoundException("Invalid session's id");
         } catch (StripeException ex) {
-            throw new RuntimeException(ex);
+            throw new StripeErrorException(ex.getMessage());
         }
     }
 }
